767c9b6fe50733ea93dc9c08677968ccbcf53ddc
Vlastimil Holer <vholer@opennebula.systems>
2019-06-21 11:18:26 +0200

    F #2799: Build Ruby gems

--- ubuntu1804.sh
+++ ubuntu1804.sh
@@ -122,6 +122,34 @@ PB_HTTP_PROXY=${HTTP_PROXY:+--http-proxy "${HTTP_PROXY}"}
 pbuilder-dist bionic amd64 create --updates-only ${PB_HTTP_PROXY}
 pbuilder-dist bionic amd64 build ${PB_HTTP_PROXY} ../*dsc
 
+################################################################################
+# Build Ruby gems packages
+################################################################################
+
+set -e
+pwd
+RUBYGEMS_DIR=$HOME/packages/rubygems
+
+# install dependencies
+"${RUBYGEMS_DIR}"/prepare.sh debian
+
+# get Gemfile, Gemfile.lock
+tar -xvf "${RPMBUILDIR}/SOURCES/${SOURCE}" \
+    -O "${NAME}-${VERSION}/share/install_gems/Gemfile" \
+    > /tmp/Gemfile
+
+tar -xvf "${RPMBUILDIR}/SOURCES/${SOURCE}" \
+    -O "${NAME}-${VERSION}/share/install_gems/Ubuntu1804/Gemfile.lock" \
+    > /tmp/Gemfile.lock
+
+# build packages
+"${RUBYGEMS_DIR}"/gemtopackage.rb -t deb \
+    --packager "${CONTACT}" \
+    --release "${PKG_VERSION}" \
+    -p "${PBUILD_DIR}" \
+    -g /tmp/Gemfile \
+    -l /tmp/Gemfile.lock
+
 # build a tar.gz with the files
 cd $PBUILD_DIR
 mkdir source
