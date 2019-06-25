0eabfd00c92361b63523566d6cf6b355424de6d5
Vlastimil Holer <vholer@opennebula.systems>
2019-06-25 10:51:44 +0200

    F #2799: Build Ruby gems

--- rubygems/build.sh
+++ rubygems/build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# -------------------------------------------------------------------------- #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
+
 set -e
 
 RUBYGEMS_DIR=${RUBYGEMS_DIR:-/root/packages/rubygems/}
@@ -34,7 +50,7 @@ elif command -v rpm >/dev/null; then
     PKG=${1:-rpm}
 fi
 
-"${RUBYGEMS_DIR}"/gemtopackage.rb \
+"${RUBYGEMS_DIR}"/gemfile2pkg \
     -t "${PKG}" \
     -p "${OUTPUT}" \
     -g "${TMP_GEMFILE}" \
